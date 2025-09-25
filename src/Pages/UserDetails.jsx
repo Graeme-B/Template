cecc3d70e2f7a07aca766efc717f0ce76a94263c
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-09-25 22:03:33 +0100

    Update user details

--- src/Pages/UserDetails.jsx
+++ src/Pages/UserDetails.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, navigation } from 'react';
+import React, { useState, useEffect, useRef, navigation } from 'react';
 import { Link } from "react-router-dom"
-import { Box, FormControl, InputLabel, Select, MenuItem, Grid } from '@mui/material'
-import TextField from '@mui/material/TextField';
+import { Box, FormControl, InputLabel, Select, MenuItem, Grid, TextField, Checkbox, FormGroup, FormControlLabel, Button, Stack } from '@mui/material'
 import $ from 'jquery';
 import { format } from 'util';
 import packageInfo from '../../package.json';
@@ -9,53 +8,27 @@ import * as Constants from '../Constants';
 import { useAuthContext } from '../AuthContext'
 
 export default function UserDetails (props) {
-//   const [country, setCountry]             = useState("");
-//   const [year, setYear]                   = useState("");
-//   const [course, setCourse]               = useState("");
-//   const [clazz, setClazz]                 = useState("");
-//   const [tableContent, setTableContent]   = useState([]);
-//   const [countries, setCountries]         = useState([]);
-//   const [years, setYears]                 = useState([]);
-//   const [courses, setCourses]             = useState([]);
-//   const [classes, setClasses]             = useState([]);
-//   const [walkList, setWalkList]           = useState([]);
-//   const [confirmIsOpen, setConfirmIsOpen] = useState(false);
-//   const [deletionId, setDeletionId]       = useState(0);
-//   const [rowsPerPage, setRowsPerPage]     = useState(25);
-//   const [currentRow, setCurrentRow]       = useState(0);
-//   const [numRows, setNumRows]             = useState(0);
-//   const [nextEnabled, setNextEnabled]     = useState(false);
-//   const [prevEnabled, setPrevEnabled]     = useState(false);
-//   const [pageNo, setPageNo]               = useState(1);
-//   const [inputPageNo, setInputPageNo]     = useState(1);
-//   const [numPages, setNumPages]           = useState(1);
-
   const {authenticated, setAuthenticated,
          name,          setName,
          email,         setEmail,
          userid,        setUserid}                          = useAuthContext();
+  const contentRef                                          = useRef();
   const [forename, setForename]                             = useState("");
   const [surname, setSurname]                               = useState("");
   const [telephoneNo, setTelephoneNo]                       = useState("");
   const [userEmail, setUserEmail]                           = useState("");
-  const [adminUser, setAdminUser]                           = useState("");
-  const [twoPhase, setTwoPhase]                             = useState("");
-  const [accountInactive, setAccountInactive]               = useState("");
-  const [toBeActivated, setToBeActivated]                   = useState("");
+  const [adminUser, setAdminUser]                           = useState(false);
+  const [twoPhase, setTwoPhase]                             = useState(false);
+  const [accountInactive, setAccountInactive]               = useState(false);
+  const [toBeActivated, setToBeActivated]                   = useState(false);
 
   const [lastLoggedIn, setLastLoggedIn]                     = useState("");
-  const [passwordResetRequested, setPasswordResetRequested] = useState("");
+  const [passwordResetRequested, setPasswordResetRequested] = useState(false);
   const [invalidLoginAttempts, setInvalidLoginAttempts]     = useState("");
-  const [emailInvalid, setEmailInvalid]                     = useState("");
-
-
-//   const hideHeader   = () => { var elem = document.getElementById("header"); elem.style.display = 'none'; }
-//   const hideFooter   = () => { var elem = document.getElementById("footer"); elem.style.display = 'none'; }
-//   const closeConfirm = () => setConfirmIsOpen(false)
-//   const openConfirm  = (walkId) => { setDeletionId(walkId); setConfirmIsOpen(true); }
+  const [emailInvalid, setEmailInvalid]                     = useState(false);
 
   useEffect(() => {
-    fetchData();
+   fetchData();
   }, []);
 
   function fetchData() {
@@ -65,18 +38,18 @@ export default function UserDetails (props) {
       xhrFields: { withCredentials: true, credentials: 'include' },
       success(json) {
         if (json["result"] === "success") {
-            setForename(json["user"]["forename"]);
-            setSurname(json["user"]["surname"]);
-            setTelephoneNo(json["user"]["telephone_no"]);
-            setUserEmail(json["user"]["email"]);
-            setAdminUser(json["user"]["admin_user"]);
-            setTwoPhase(json["user"]["two_phase"]);
-            setAccountInactive(json["user"]["account_inactive"]);
-            setToBeActivated(json["user"]["to_be_activated"]);
-            setLastLoggedIn(json["user"]["last_logged_in"]);
-            setPasswordResetRequested(json["user"]["password_reset_requested"]);
-            setInvalidLoginAttempts(json["user"]["invalid_login_attempts"]);
-            setEmailInvalid(json["user"]["email_invalid"]);
+          setForename(json["user"]["forename"]);
+          setSurname(json["user"]["surname"]);
+          setTelephoneNo(json["user"]["telephone_no"]);
+          setUserEmail(json["user"]["email"]);
+          setAdminUser(json["user"]["admin_user"] == "1" ? true : false);
+          setTwoPhase(json["user"]["two_phase"] == "1" ? true : false);
+          setAccountInactive(json["user"]["account_inactive"] == "1" ? true : false);
+          setToBeActivated(json["user"]["to_be_activated"] == "1" ? true : false);
+          setLastLoggedIn(json["user"]["last_logged_in"]);
+          setPasswordResetRequested(json["user"]["password_reset_requested"] == "1" ? true : false);
+          setInvalidLoginAttempts(json["user"]["invalid_login_attempts"]);
+          setEmailInvalid(json["user"]["email_invalid"] == "1" ? true : false);
         }
       }
     });
@@ -95,16 +68,16 @@ export default function UserDetails (props) {
      setUserEmail(event.target.value);
   };
   const handleAdminUserChange = event => {
-     setAdminUser(event.target.value);
+     setAdminUser(!adminUser);
   };
   const handleTwoPhaseChange = event => {
-     setTwoPhase(event.target.value);
+     setTwoPhase(!twoPhase);
   };
   const handleAccountInactiveChange = event => {
-     setAccountInactive(event.target.value);
+     setAccountInactive(!accountInactive);
   };
   const handleToBeActivatedChange = event => {
-     setToBeActivated(event.target.value);
+     setToBeActivated(!toBeActivated);
   };
 
   {/*
@@ -147,7 +120,6 @@ export default function UserDetails (props) {
     overflow: auto;
     background: pink;
   };
-
   const contentStyle = {
     display: "flex",
     flexDirection: "row",
@@ -166,7 +138,39 @@ export default function UserDetails (props) {
     padding: "0 10px 10px 10px",
     margin: "20px"
   }
+  const wrapperStyle = {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    margin: "20px"
+  }
   */}
+
+{/*
+  const contentStyle = {
+    display: "flex",
+    flexDirection: "column",
+    maxHeight: 'calc(100vh - 200px)',
+    overflow: "scroll"
+  }
+  const contentStyle = {
+    flex: 1;
+    overflow: auto;
+    background: pink;
+  };
+  const contentStyle = {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#00FF00",
+    maxHeight: 'calc(100vh - 200px)',
+    overflow: "scroll"
+  }
+  const contentStyle = {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#00FF00"
+  }
+
   const wrapperStyle = {
     height: "100%",
     display: "flex",
@@ -174,12 +178,6 @@ export default function UserDetails (props) {
     margin: "20px"
   }
 
-  const contentStyle = {
-    display: "flex",
-    flexDirection: "column",
-    maxHeight: 'calc(100vh - 200px)',
-    overflow: "scroll"
-  }
   const headerStyle = {
     flex: "none",
   }
@@ -191,158 +189,119 @@ export default function UserDetails (props) {
     padding: "10px 10px 10px 10px",
   }
 
+  */}
+  const wrapperStyle = {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    margin: "20px"
+  }
+
+  const contentStyle = {
+    border: '1px solid #fff',
+    overflow: "hidden"
+  }
+
   return (
-    <div>
-      <div d="wrapper" style={wrapperStyle}>
-        <div id="header" style={headerStyle}>
-        </div>
-
-        <div id="content" style={contentStyle}>
-           Hello, world from the details page of user {userid}!
-
-           <TextField
+    <div >
+      <div style={wrapperStyle}>
+        <div id="content" ref={contentRef} style={contentStyle}>
+          <FormGroup>
+            <TextField
              autoFocus
              margin="dense"
              id="forename"
              label="Forename"
              type="text"
              fullWidth
-             variant="standard"
+             variant="outlined"
              value={forename}
              onChange={handleForenameChange}
              inputProps={{maxLength: Constants.FORENAME_LEN}}
-           />
-           <TextField
-             autoFocus
+            />
+            <TextField
              margin="dense"
              id="surname"
              label="Surname"
              type="text"
              fullWidth
-             variant="standard"
+             variant="outlined"
              value={surname}
              onChange={handleSurnameChange}
              inputProps={{maxLength: Constants.SURNAME_LEN}}
-           />
-           <TextField
-             autoFocus
+            />
+            <TextField
              margin="dense"
              id="telephoneNo"
              label="Telephone No"
              type="text"
              fullWidth
-             variant="standard"
+             variant="outlined"
              value={telephoneNo}
              onChange={handleTelephoneNoChange}
              inputProps={{maxLength: Constants.TELNO_LEN}}
-           />
-           <TextField
-             autoFocus
+            />
+            <TextField
              margin="dense"
              id="email"
              label="Email"
              type="text"
              fullWidth
-             variant="standard"
+             variant="outlined"
              value={userEmail}
              onChange={handleUserEmailChange}
              inputProps={{maxLength: Constants.EMAIL_LEN}}
-           />
-           <TextField
-             autoFocus
-             margin="dense"
-             id="adminUser"
-             label="Admin user"
-             type="text"
-             fullWidth
-             variant="standard"
-             value={adminUser}
-             onChange={handleAdminUserChange}
-             inputProps={{maxLength: Constants.ADMIN_USER_LEN}}
-           />
-           <TextField
-             autoFocus
-             margin="dense"
-             id="twoPhase"
-             label="Two phase"
-             type="text"
-             fullWidth
-             variant="standard"
-             value={twoPhase}
-             onChange={handleTwoPhaseChange}
-             inputProps={{maxLength: Constants.TWO_PHASE_LEN}}
-           />
-           <TextField
-             autoFocus
-             margin="dense"
-             id="accountInactive"
-             label="Account inactive"
-             type="text"
-             fullWidth
-             variant="standard"
-             value={accountInactive}
-             onChange={handleAccountInactiveChange}
-             inputProps={{maxLength: Constants.ACCOUNT_INACTIVE_LEN}}
-           />
-           <TextField
-             autoFocus
-             margin="dense"
-             id="toBeActivated"
-             label="To be activated"
-             type="text"
-             fullWidth
-             variant="standard"
-             value={toBeActivated}
-             onChange={handleToBeActivatedChange}
-             inputProps={{maxLength: Constants.TO_BE_ACTIVATED_LEN}}
-           />
-           <TextField
-             autoFocus
-             margin="dense"
-             id="lastLoggedIn"
-             label="Last logged in"
-             type="text"
-             fullWidth
-             variant="standard"
-             value={lastLoggedIn}
-             readOnly
-           />
-           <TextField
-             autoFocus
-             margin="dense"
-             id="passwordResetRequested"
-             label="Password reset requested"
-             type="text"
-             fullWidth
-             variant="standard"
-             value={passwordResetRequested}
-             readOnly
-           />
-           <TextField
-             autoFocus
-             margin="dense"
-             id="invalidLoginAttempts"
-             label="Invalid login attempts"
-             type="text"
-             fullWidth
-             variant="standard"
-             value={invalidLoginAttempts}
-             readOnly
-           />
-           <TextField
-             autoFocus
-             margin="dense"
-             id="emailInvalid"
-             label="Email Invalid"
-             type="text"
-             fullWidth
-             variant="standard"
-             value={emailInvalid}
-             readOnly
-           />
-        </div>
-
-        <div id="footer" style={footerStyle}>
+            />
+            {authenticated == Constants.USER_TYPE_ADMIN && (
+              <div>
+                <TextField
+                 margin="dense"
+                 id="lastLoggedIn"
+                 label="Last logged in"
+                 type="text"
+                 fullWidth
+                 variant="outlined"
+                 value={lastLoggedIn}
+                 readOnly
+                />
+                <TextField
+                 margin="dense"
+                 id="invalidLoginAttempts"
+                 label="Invalid login attempts"
+                 type="text"
+                 fullWidth
+                 variant="outlined"
+                 value={invalidLoginAttempts}
+                 readOnly
+                />
+               <Box display="flex" flexWrap="wrap" gap={2}>
+                 <FormControlLabel control={<Checkbox tabIndex={0} id="adminUser" checked={adminUser} onChange={handleAdminUserChange}/>} label="Admin user" />
+                 <FormControlLabel control={<Checkbox tabIndex={0} id="twoPhase" checked={twoPhase} onChange={handleTwoPhaseChange}/>} label="Two phase" />
+                 <FormControlLabel control={<Checkbox tabIndex={0} id="accountInactive" checked={accountInactive} onChange={handleAccountInactiveChange}/>} label="Account inactive" />
+                 <FormControlLabel control={<Checkbox tabIndex={0} id="toBeActivated" checked={toBeActivated} onChange={handleToBeActivatedChange}/>} label="To be activated" />
+                 <FormControlLabel control={<Checkbox id="passwordResetRequested" checked={passwordResetRequested}/>} label="Password reset requested" />
+                 <FormControlLabel control={<Checkbox id="emailInvalid" checked={emailInvalid}/>} label="Email Invalid" />
+               </Box>
+             </div>
+           )}
+           <Box>
+           <Button variant="contained"
+             onClick={() => {
+               alert('Save');
+             }}
+           >
+             Save
+           </Button>
+           &nbsp;
+           <Button variant="contained"
+             onClick={() => {
+               alert('Cancel');
+             }}
+           >
+             Cancel
+           </Button>
+           </Box>
+        </FormGroup>
         </div>
       </div>
     </div>
